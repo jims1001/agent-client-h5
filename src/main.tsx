c422848c49d44bd715fc69417bb2d559a3828386
jed <166913797+jims1001@users.noreply.github.com>
2025-05-20 16:49:07 +0400

    add support routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
+import { BrowserRouter } from "react-router";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 import App from './App.tsx'
 import React from 'react'
+import {Route, Routes} from "react-router-dom";
 window.isInitSocket = false;
 window.isReadySocket = false;
-
-
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" Component={App} />
+          </Routes>
+
+      </BrowserRouter>
+
   </StrictMode>,
 )
---
+++ src/route.tsx
@@ -0,0 +1,15 @@
+import App from "@/App";
+import React from "react";
+
+const routes = [
+    {
+        "name": "aboutHobo",
+        "path": "/aboutHobo",
+        "component": <App></App>,
+        "children": [],
+        "customPath": "/aboutHobo"
+    },
+]
+
+
+export  {routes}
